c8626cfddffb237c30b2a985d505dc780a03669c
Mojtaba Sabbagh <mojtaba.sabbagh@yahoo.com>
2023-01-30 09:51:30 +0330

    Switch back to run server.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ python manage.py migrate --no-input
 mkdir -p /app/static
 python manage.py collectstatic
 
-gunicorn meetrest.wsgi:application --bind 0.0.0.0:8000
-#python manage.py runserver 0.0.0.0:8000
+#gunicorn meetrest.wsgi:application --bind 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000
